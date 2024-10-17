c225a0be2f383aa8ea2e662d16a15ce1e99caa89
jihoon kwak <kwakjihoon1001@gmail.com>
2024-10-17 18:08:54 +0900

    feat: explanation 컴포넌트 추가 및 to do list 작성

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/header/Header";
 import Product from "./components/product/Product";
 import Footer from "./components/footer/Footer";
 import Layout from "./components/layout/Layout";
+import Explanation from "./components/explanation/Explanation";
 
 export default function App() {
   return (
     <>
       <Header />
       <Layout>
+      <Explanation />
       <Product /> 
       </Layout>     
       <Footer />
---
+++ src/components/explanation/Explanation.jsx
@@ -0,0 +1,33 @@
+export default function Explanation() {
+    return (
+        <>
+        <div className="Explain__container">
+            <p className="Basic__explanation">
+            "Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Curabitur vel nulla a orci vestibulum dignissim. 
+            Nam eget neque id ex varius sagittis. Donec non tortor massa. 
+            Aenean aliquet metus id est cursus, nec convallis eros pellentesque. 
+            Vivamus tincidunt libero nec sem bibendum, sit amet ullamcorper tortor pretium. Praesent at suscipit purus,
+            at vehicula est. Etiam imperdiet, libero a tempus tincidunt, nisl leo elementum risus, eget feugiat sem enim a augue. Cras sed scelerisque magna. 
+            Duis finibus, dolor id commodo venenatis, turpis ipsum efficitur libero, ut ultricies arcu ligula sed quam. Ut vehicula augue sit amet enim venenatis faucibus."
+            </p>
+        </div>
+        <div className="Explain__container">
+            <p className="Usage__explanation">
+            "Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Curabitur vel nulla a orci vestibulum dignissim. 
+            Nam eget neque id ex varius sagittis. Donec non tortor massa. 
+            Aenean aliquet metus id est cursus, nec convallis eros pellentesque. 
+            Vivamus tincidunt libero nec sem bibendum, sit amet ullamcorper tortor pretium. Praesent at suscipit purus,
+            at vehicula est. Etiam imperdiet, libero a tempus tincidunt, nisl leo elementum risus, eget feugiat sem enim a augue. Cras sed scelerisque magna. 
+            Duis finibus, dolor id commodo venenatis, turpis ipsum efficitur libero, ut ultricies arcu ligula sed quam. Ut vehicula augue sit amet enim venenatis faucibus."
+            </p>  
+        </div>
+        </> 
+    )
+}
+
+//TODO: 전체 뷰 크기 수정
+//TODO: 설명글 추가 및 css 수정 요망
