e170cd3d305fe53aaa4491232e04eb416020d857
jihoon kwak <kwakjihoon1001@gmail.com>
2024-10-19 23:53:47 +0900

    fix: explanation 영어 문단 한글로 수정(임시 데이터)

--- src/components/explanation/Explanation.jsx
+++ src/components/explanation/Explanation.jsx
@@ -3,26 +3,19 @@ export default function Explanation() {
         <>
         <div className="Explain__container">
             <p className="Basic__explanation">
-            "Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit. 
-            Curabitur vel nulla a orci vestibulum dignissim. 
-            Nam eget neque id ex varius sagittis. Donec non tortor massa. 
-            Aenean aliquet metus id est cursus, nec convallis eros pellentesque. 
-            Vivamus tincidunt libero nec sem bibendum, sit amet ullamcorper tortor pretium. Praesent at suscipit purus,
-            at vehicula est. Etiam imperdiet, libero a tempus tincidunt, nisl leo elementum risus, eget feugiat sem enim a augue. Cras sed scelerisque magna. 
-            Duis finibus, dolor id commodo venenatis, turpis ipsum efficitur libero, ut ultricies arcu ligula sed quam. Ut vehicula augue sit amet enim venenatis faucibus."
+            쿠팡 구매 추천 사이트에 오신 것을 환영합니다! 
+            이 플랫폼은 여러분의 선호도에 맞춰 최적의 상품을 추천해드리기 위해 제작되었습니다. 
+            인기 있는 상품부터 맞춤형 추천까지, 쿠팡에 있는 다양한 품목을 분석하여 여러분께 가장 적합한 제품을 제안해 드립니다. 
+            페이지를 탐색하며 원하는 항목을 입력하고, 나머지는 저희가 처리해 드립니다!
             </p>
         </div>
         <div className="Explain__container">
             <p className="Usage__explanation">
-            "Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit. 
-            Curabitur vel nulla a orci vestibulum dignissim. 
-            Nam eget neque id ex varius sagittis. Donec non tortor massa. 
-            Aenean aliquet metus id est cursus, nec convallis eros pellentesque. 
-            Vivamus tincidunt libero nec sem bibendum, sit amet ullamcorper tortor pretium. Praesent at suscipit purus,
-            at vehicula est. Etiam imperdiet, libero a tempus tincidunt, nisl leo elementum risus, eget feugiat sem enim a augue. Cras sed scelerisque magna. 
-            Duis finibus, dolor id commodo venenatis, turpis ipsum efficitur libero, ut ultricies arcu ligula sed quam. Ut vehicula augue sit amet enim venenatis faucibus."
+            이용 방법은 간단합니다. 먼저 관심 있는 상품 카테고리를 입력하세요. 
+            예를 들어 전자제품, 의류, 생활용품 등이 있습니다. 
+            다음으로, 해당 카테고리에 대한 선호도를 별점으로 표시해 주세요. 
+            마지막으로, 추천받고 싶은 아이템의 개수를 선택하고 버튼을 눌러 맞춤형 상품 추천을 받아보세요. 
+            매우 간단합니다!
             </p>  
         </div>
         </> 
