c70eaf599c9e5722d28b4c5f4163b1800e51eeee
minggichae <che08180@naver.com>
2024-11-14 17:40:41 +0900

    feat: sample image file, fix: gpt model id change, image tesing

--- src/components/explanation/Explanation.jsx
+++ src/components/explanation/Explanation.jsx
@@ -1,43 +1,40 @@
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
 
-export default function Explanation( {scrollRef} ) {
-
-    const handleScroll = () => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollIntoView({ behavior: "smooth" });
-            {
-              /*
+export default function Explanation({ scrollRef }) {
+  const handleScroll = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+      {
+        /*
                         .current: DOM 요소의 값을 저장할 수 있는 프로퍼티 
                         scrollIntoView: 해당 요소로 이동, current 프로퍼티에 저장된 DOM 요소의
                         객체값으로 view 이동
                     */
-            }
+      }
     }
-}
-    return (
-        <>
-        <div className="Explain__container">
-            <p className="Basic__explanation">        
-            COUrse는 여러분들의 고민하는 시간을 줄여드립니다!
-            원하는 요구 사항을 AI 기반으로 분석하고, 
-            최적의 상품을 직관적으로 추천해드릴게요! 
-            </p>
-    
-        </div>
-        <div className="Explain__container">
-            <p className="Usage__explanation">
-            카테고리를 입력하고, 최소 별점 지정을 통해 필터링 해주세요. 
-            예를 들어 3점을 눌렀다면, 별점이 3점 이상인 제품들을 볼 수 있어요. 
-            마지막으로 추천 받고 싶은 아이템의 개수를 선택하고 버튼을 누르시면 완료입니다!
-            그 뒤의 일은 COUrse에게 맡기세요 :)
-            </p> 
-        </div> 
+  };
+  return (
+    <>
+      <div className="Explain__container">
+        <p className="Basic__explanation">
+          COUrse는 여러분들의 고민하는 시간을 줄여드립니다! 원하는 요구 사항을
+          AI 기반으로 분석하고, 최적의 상품을 직관적으로 추천해드릴게요!
+        </p>
+      </div>
+      <div className="Explain__container">
+        <p className="Usage__explanation">
+          카테고리를 입력하고, 최소 별점 지정을 통해 필터링 해주세요. 예를 들어
+          3점을 눌렀다면, 별점이 3점 이상인 제품들을 볼 수 있어요. 마지막으로
+          추천 받고 싶은 아이템의 개수를 선택하고 버튼을 누르시면 완료입니다! 그
+          뒤의 일은 COUrse에게 맡기세요 :)
+        </p>
+      </div>
 
-        <div className="Explain__container">
+      <div className="Explain__container">
         <button className="Custom-btn Scroll__button" onClick={handleScroll}>
           정보 기입 하러 가기!
-        </button>   
-        </div> 
-        </> 
-    )
-  }
+        </button>
+      </div>
+    </>
+  );
+}
--- src/components/productresult/ProductResult.jsx
+++ src/components/productresult/ProductResult.jsx
@@ -9,6 +9,10 @@ export default function ProductResult({
   const [response, setResponse] = useState(""); // 응답한 내용 저장하는 state
   const [loading, setLoading] = useState(false); // 추천 상품이 나오기 전 로딩중이라는 문구를 띄우기 위한 state
 
+  const ErrorImageHandler = (e) => {
+    e.target.onerror = null; // 무한 루프 방지
+    e.target.src = "src/image/error.jpg";
+  };
   // 버튼 클릭시 실행할 함수
   const CallGPT = async (e) => {
     // 카테고리 값이 없으면 return 실행
@@ -28,7 +32,7 @@ export default function ProductResult({
           Authorization: `Bearer ${apiKey}`,
         },
         body: JSON.stringify({
-          model: "ft:gpt-4o-mini-2024-07-18:personal::AQZFej9V", // 사용할 AI 모델
+          model: "ft:gpt-4o-mini-2024-07-18:personal::ASfGAmSb", // 사용할 AI 모델
           messages: [
             // 메세지 role과 content를 여러개 작성이 가능함.
             {
@@ -36,7 +40,7 @@ export default function ProductResult({
               content: `당신은 상품을 추천해주는 전문가이다. 상품의 카테고리를 입력받으면 그 카테고리에 알맞는 제품의 상품 정보를 생성하여 출력해라.
               입력받은 카테고리는 ${categoryValue}이고, 별점은 ${starScore}점 이상, 제품 추천 수는 ${printCount}개로 고정해라.
               별점을 가져와서 나타낼땐 4/5 이런식으로 나타내라.
-              그리고, 카테고리에 관련된 제품만 추천해주고 다른 제품은 추천하지 마라. 예를 들면 채소를 입력하면 상추, 깻잎 이런류만 알려주고 채소 다지는 기계 이런 답변은 하지마라.`,
+              카테고리에 관련된 제품만 추천해주고 다른 제품은 추천하지 마라. 예를 들면 채소를 입력하면 상추, 깻잎 이런류만 알려주고 채소 다지는 기계 이런 답변은 하지마라.`,
             },
             {
               role: "user",
@@ -44,7 +48,7 @@ export default function ProductResult({
             },
             {
               role: "assistant",
-              content: `제품명, 가격, 별점 이 세가지 외의 답변은 하지마. 가격은 한국 환율을 기준으로 잡아줘. 제품의 추천 개수는 ${printCount}를 절대 넘어가서는 안돼.`,
+              content: `이미지, 제품명, 가격, 별점 이 네가지 외의 답변은 하지마. 가격은 한국 환율을 기준으로 잡아줘. 제품의 추천 개수는 ${printCount}를 절대 넘어가서는 안돼.`,
             },
           ],
           temperature: 0, // 모델의 출력 다양성
@@ -87,24 +91,63 @@ export default function ProductResult({
           <div className="Product__Result__Container">
             {response &&
               response.split("\n").map((product, index) => {
+                console.log(response);
                 console.log(`${index}번:`, product);
-                const [name, price, rating] = product.split(", ");
+
+                // let image = null;
+                let name = "제품명";
+                let price = "가격";
+                let rating = "별점";
+
+                // if (product.includes("이미지:")) {
+                //   image = product.split("이미지: ")[1]?.split(",")[0].trim();
+                // }
+                if (product.includes("제품명:")) {
+                  name = product.split("제품명: ")[1]?.split(",")[0].trim();
+                }
+                if (product.includes("가격:")) {
+                  price = product.split("가격: ")[1]?.split("원")[0].trim();
+                }
+                if (product.includes("별점:")) {
+                  rating = product.split("별점: ")[1]?.split(",")[0].trim();
+                }
                 /*
-                name, price, rating에 각 제품명, 가격, 별점이 들어감 
-                product.split(", ")을 통해 데이터를 가져와서 product 변수에 넣은 문장을 ","으로 구분후 저장
-                e.g. "제품명: 감", "가격: 3,500원", "별점: 5"를 ,을 기준으로 나눈 다음
-                name = 제품명: 감, price = 가격: 3,500원, rating = 별점: 5를 저장
+                이미지, 제품명, 가격, 별점이 들어갈 변수 생성 -> image, name, price, rating
+
+                let으로 선언한 이유는 내용이 계속 들어가기에 const로 하면 내용을 바꿀 수 없기 때문
+                if문을 통해 문자열에서 해당하는 텍스트를 포함할 시 각 해당하는 변수에 값 할당
+                split을 통해 이미지, 제품명, 가격, 별점이라는 텍스트를 구분 짓고 해당하는 문자열을 가져온 다음
+                가져온 문자열은 배열의 두번째 원소로 저장되기에 [1]을 사용해 문자열을 다시 가져오고,
+                split을 통해 다시 문자열을 나누고 첫번째 원소를 가져온뒤 trim을 통해 공백을 제거한다.
+
+                e.g. 더미데이터의 내용이 "이미지: 귤.jpg, 제품명: 귤, 가격: 3,000원, 별점: 4" 일때
+                product.split("이미지: ") -> ["", "example.jpg, 제품명: 귤, 가격: 3,000원, 별점: 4"]
+                [1]?. -> ["example.jpg, 제품명: 귤, 가격: 3,000원, 별점: 4"] (여기서 ?.는 비어있지 않은 경우에만 실행되게끔 해둔 상태)
+                - ?. = 옵셔널 체이닝이란? ?. 앞에 항목이 undefined 또는 null인 경우 undefined를 반환
+                split(",")과 split("원") -> ["example.jpg", " 제품명: 귤", " 가격: 3,000원", " 별점: 4"](가격은 "원"으로 한 이유는 ","으로 할 시 3,000원에서 3만 가져오기 때문)
+                [0] -> 배열의 첫번째 원소 선택
+                trim() -> 공백 제거
+                결과: image = 귤.jpg, name = 귤, price = 3,000, rating = 4
                 */
+                // console.log(image);
                 console.log(name);
                 console.log(price);
                 console.log(rating);
                 return (
                   <div className="Product__Result" key={index}>
-                    <p>{name}</p>
-                    <p>{price}</p>
-                    <p>{rating}</p>
+                    <img
+                      src="src/image/error.jpg"
+                      alt="제품 이미지"
+                      // onError={ErrorImageHandler}
+                      width={75}
+                      height={75}
+                    />
+                    <p>제품명: {name}</p>
+                    <p>가격: {price}</p>
+                    <p>별점: {rating}</p>
                   </div>
                 );
+
                 /*
                 response를 불러와서 결과값을 가져옴.
                 .split("\n")을 통해 줄바꿈으로 배열을 나눔. => 여기서 split은 문자열을 나눌 때 사용되는 JavaScript의 문자열 메서드이다.
